018a42f56fbba33d03aa27535906860aba76a01c
rizankhan <rizankhan837@gmail.com>
2023-06-16 23:28:57 +0500

    Some Changes In Wallet.jsx

--- Week1 - ecdsa-node/client/src/Wallet.jsx
+++ Week1 - ecdsa-node/client/src/Wallet.jsx
@@ -1,9 +1,17 @@
 import server from "./server";
+import * as secp256k1 from "ethereum-cryptography/secp256k1";
+import {toHex} from "ethereum-cryptography/utils"
+import {keccak256} from "ethereum-cryptography/keccak"
 
-function Wallet({ address, setAddress, balance, setBalance }) {
+function Wallet({ address, setAddress, balance, setBalance, privateKey, setPrivateKey }) {
   async function onChange(evt) {
-    const address = evt.target.value;
+    const privateKey = evt.target.value;
+    setPrivateKey(privateKey);
+
+    const hash = keccak256(toHex(secp256k1.getPublicKey(privateKey)).slice(1));
+    const address = toHex(hash.slice(-20));
     setAddress(address);
+
     if (address) {
       const {
         data: { balance },
@@ -19,9 +27,13 @@ function Wallet({ address, setAddress, balance, setBalance }) {
       <h1>Your Wallet</h1>
 
       <label>
-        Wallet Address
-        <input placeholder="Type an address, for example: 0x1" value={address} onChange={onChange}></input>
+        Private Key
+        <input placeholder="Private Key" value={privateKey} onChange={onChange}></input>
       </label>
+
+      <div>
+        Public Key: <span className="address">{address}</span>
+      </div>
 
       <div className="balance">Balance: {balance}</div>
     </div>
--- Week1 - ecdsa-node/server/scripts/generate.js
+++ Week1 - ecdsa-node/server/scripts/generate.js
@@ -1,8 +1,17 @@
 const {secp256k1} = require("ethereum-cryptography/secp256k1");
 const {toHex} = require('ethereum-cryptography/utils');
+const {keccak256} = require('ethereum-cryptography/keccak');
 
 const privateKey = secp256k1.utils.randomPrivateKey();
 const publicKey = secp256k1.getPublicKey(privateKey);
 
-console.log('Public Key: ' ,toHex(publicKey));
+function getAddress(publicKey) {
+    let hash = keccak256(publicKey.slice(1));
+    return hash.slice(-20);
+ }
+
+const address = '0x' + toHex(getAddress(publicKey));
+
+console.log('Public Key: ' , toHex(publicKey));
 console.log('Private Key: ' , toHex(privateKey));
+console.log('Address:  ' , address);
